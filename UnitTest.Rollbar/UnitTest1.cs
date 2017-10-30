bfd127359ac9817ebae8b7df32bf4ef1dcce224a
akornich (Wide Spectrum Computing LLC) <akornich@gmail.com>
2017-10-30 14:33:15 -0700

    seeded unit test project;

---
+++ UnitTest.Rollbar/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace UnitTest.Rollbar
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
