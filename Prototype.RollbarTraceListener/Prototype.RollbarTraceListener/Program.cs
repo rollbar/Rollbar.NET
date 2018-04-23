e5ab9aff165cc6663d6beb947e35dd71e7e3277e
akornich (Wide Spectrum Computing LLC) <akornich@gmail.com>
2018-04-23 13:22:01 -0700

    added Prototype.RollbarTraceListener with basic text-file trace listener enabled.

---
+++ Prototype.RollbarTraceListener/Prototype.RollbarTraceListener/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.RollbarTraceListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.WriteLine("Tracing something...");
+            Trace.TraceError(new Exception().ToString());
+        }
+    }
+}
