b098124a1f175f8ef41a6b4eaf69d0174cd48229
Andrey Kornich <akornich@gmail.com>
2018-11-12 21:16:16 -0800

    Prepping for v2.0.0 release
          - resolve #209: Improve logging methods' performance (short constant duration regardless of payload type/size)
          - resolve #210: Simplify logging API
          - resolve #211: Update performance/API-simplification related documentation

--- Rollbar/RollbarUtil.cs
+++ Rollbar/RollbarUtil.cs
@@ -3,42 +3,16 @@
     using Rollbar.DTOs;
     using System;
     using System.Collections.Generic;
-    using System.Text;
 
     internal static class RollbarUtil
     {
-        //public static IAsyncLogger LogUsingProperObjectDiscovery(IRollbar rollbar, ErrorLevel level, object obj, IDictionary<string, object> custom = null)
-        //{
-        //    if (rollbar.Config.LogLevel.HasValue && level < rollbar.Config.LogLevel.Value)
-        //    {
-        //        // nice shortcut:
-        //        return rollbar;
-        //    }
 
-        //    Data data = obj as Data;
-        //    if (data != null)
-        //    {
-        //        data.Level = level;
-        //        return rollbar.Log(data);
-        //    }
-        //    System.Exception exception = obj as System.Exception;
-        //    if (exception != null)
-        //    {
-        //        Body exceptionBody = new Body(exception);
-        //        Data exceptionData = new Data(rollbar.Config, exceptionBody, custom);
-        //        rollbar.Log(exceptionData);
-        //        return rollbar;
-        //    }
-        //    ITraceable traceable = obj as ITraceable;
-        //    if (traceable != null)
-        //    {
-        //        return rollbar.Log(level, traceable.TraceAsString(), custom);
-        //    }
-
-        //    return rollbar.Log(level, obj.ToString(), custom);
-        //}
-
-        public static Data PackageAsPayloadData(IRollbarConfig rollbarConfig, ErrorLevel level, object obj, IDictionary<string, object> custom = null)
+        public static Data PackageAsPayloadData(
+            IRollbarConfig rollbarConfig, 
+            ErrorLevel level, 
+            object obj, 
+            IDictionary<string, object> custom = null
+            )
         {
             if (rollbarConfig.LogLevel.HasValue && level < rollbarConfig.LogLevel.Value)
             {
