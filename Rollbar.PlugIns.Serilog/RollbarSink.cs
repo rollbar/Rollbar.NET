3f1477ffc81c14b5c795fe088537c474da232028
Andrey Kornich <akornich@gmail.com>
2018-11-17 01:26:11 -0800

    - ref #190: Make Rollbar.NET available as a Serilog sink

---
+++ Rollbar.PlugIns.Serilog/RollbarSink.cs
@@ -0,0 +1,139 @@
+﻿namespace Rollbar.PlugIns.Serilog
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Serilog.Core;
+    using global::Serilog.Events;
+    using Serilog;
+
+    public class RollbarSink
+        : ILogEventSink
+    {
+        private readonly IFormatProvider _formatProvider;
+        private readonly IRollbarConfig _rollbarConfig;
+        private readonly IAsyncLogger _rollbarAsyncLogger;
+        private readonly ILogger _rollbarLogger;
+
+        public RollbarSink(
+            string rollbarAccessToken, 
+            string rollbarEnvironment,
+            TimeSpan? rollbarBlockingLoggingTimeout,
+            IFormatProvider formatProvider
+            )
+            : this(
+                  new RollbarConfig(rollbarAccessToken) { Environment = rollbarEnvironment}, 
+                  rollbarBlockingLoggingTimeout,
+                  formatProvider
+                  )
+        {
+        }
+
+        public RollbarSink(
+            IRollbarConfig rollbarConfig,
+            TimeSpan? rollbarBlockingLoggingTimeout,
+            IFormatProvider formatProvider
+            )
+        {
+            this._rollbarConfig = rollbarConfig;
+
+            IRollbar rollbar = RollbarFactory.CreateNew().Configure(this._rollbarConfig);
+
+            if (rollbarBlockingLoggingTimeout.HasValue)
+            {
+                this._rollbarLogger = rollbar.AsBlockingLogger(rollbarBlockingLoggingTimeout.Value);
+            }
+            else
+            {
+                this._rollbarAsyncLogger = rollbar;
+            }
+
+            _formatProvider = formatProvider;
+
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent == null)
+            {
+                return;
+            }
+
+            ErrorLevel rollbarLogLevel;
+            switch (logEvent.Level)
+            {
+                case LogEventLevel.Fatal:
+                    rollbarLogLevel = ErrorLevel.Critical;
+                    break;
+                case LogEventLevel.Error:
+                    rollbarLogLevel = ErrorLevel.Error;
+                    break;
+                case LogEventLevel.Warning:
+                    rollbarLogLevel = ErrorLevel.Warning;
+                    break;
+                case LogEventLevel.Information:
+                    rollbarLogLevel = ErrorLevel.Info;
+                    break;
+                case LogEventLevel.Verbose:
+                case LogEventLevel.Debug:
+                default:
+                    rollbarLogLevel = ErrorLevel.Debug;
+                    break;
+            }
+
+            DTOs.Body rollbarBody = null;
+            if (logEvent.Exception != null)
+            {
+                rollbarBody = new DTOs.Body(logEvent.Exception);
+            }
+            else
+            {
+                string message = logEvent.RenderMessage(this._formatProvider);
+                rollbarBody = new DTOs.Body(message);
+            }
+
+            int customCapacity = 1;
+            if (logEvent.Properties != null)
+            {
+                customCapacity += logEvent.Properties.Count;
+            }
+            IDictionary<string, object> custom = new Dictionary<string, object>(customCapacity);
+            if (logEvent.Properties != null)
+            {
+                foreach (var property in logEvent.Properties)
+                {
+                    custom[property.Key] = property.Value.ToString();
+                }
+            }
+            custom["Serilog.LogEvent.Timestamp"] = logEvent.Timestamp;
+
+            DTOs.Data rollbarData = new DTOs.Data(this._rollbarConfig, rollbarBody, custom)
+            {
+                Level = rollbarLogLevel
+            };
+
+            this.ReportToRollbar(rollbarData);
+        }
+
+        private void ReportToRollbar(DTOs.Data rollbarData)
+        {
+            if (this._rollbarAsyncLogger != null)
+            {
+                RollbarSink.ReportToRollbar(this._rollbarAsyncLogger, rollbarData);
+            }
+            else if (this._rollbarLogger != null)
+            {
+                RollbarSink.ReportToRollbar(this._rollbarLogger, rollbarData);
+            }
+        }
+
+        private static void ReportToRollbar(ILogger logger, DTOs.Data rollbarData)
+        {
+            logger.Log(rollbarData);
+        }
+
+        private static void ReportToRollbar(IAsyncLogger logger, DTOs.Data rollbarData)
+        {
+            logger.Log(rollbarData);
+        }
+    }
+}
---
+++ Rollbar.PlugIns.Serilog/RollbarSinkExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Rollbar.PlugIns.Serilog
+{
+    using global::Serilog;
+    using global::Serilog.Configuration;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class RollbarSinkExtensions
+    {
+        public static LoggerConfiguration RollbarSink(
+                  this LoggerSinkConfiguration loggerConfiguration,
+                  string rollbarAccessToken,
+                  string rollbarEnvironment,
+                  TimeSpan? rollbarBlockingLoggingTimeout,
+                  IFormatProvider formatProvider = null)
+        {
+            IRollbarConfig config = new RollbarConfig(rollbarAccessToken)
+            {
+                Environment = rollbarEnvironment,
+            };
+
+            return loggerConfiguration.RollbarSink(config, rollbarBlockingLoggingTimeout, formatProvider);
+        }
+
+        public static LoggerConfiguration RollbarSink(
+                  this LoggerSinkConfiguration loggerConfiguration,
+                  IRollbarConfig rollbarConfig,
+                  TimeSpan? rollbarBlockingLoggingTimeout,
+                  IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new RollbarSink(rollbarConfig, rollbarBlockingLoggingTimeout, formatProvider));
+        }
+    }
+}
