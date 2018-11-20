44b9899ed9ddb81232a96844f37b37922fdbb587
Andrey Kornich <akornich@gmail.com>
2018-11-20 15:55:18 -0800

    - ref #217: Create a sample app of using RollbarSerilogSink:     created initial console app that uses Serilog and its Console sink.

---
+++ Sample.PlugIns.Serilog/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using Serilog;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                 .MinimumLevel.Information()
+                 .WriteTo.Console()
+                 .CreateLogger();
+
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+
+            log.Information("Processed {@Position} in {Elapsed:000} ms.", position, elapsedMs);
+        }
+    }
+}
