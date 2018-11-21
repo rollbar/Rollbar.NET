0cafa2684a16f746667f12ac783cc3590cd8d395
Andrey Kornich <akornich@gmail.com>
2018-11-20 20:38:56 -0800

    - resolve #217: Create a sample app of using RollbarSerilogSink:     added RollbarSink to Serilog.

--- Sample.PlugIns.Serilog/ConsoleApp/Program.cs
+++ Sample.PlugIns.Serilog/ConsoleApp/Program.cs
@@ -2,15 +2,25 @@
 {
     using System;
     using Serilog;
+    using Rollbar;
+    using Rollbar.PlugIns.Serilog;
 
     public class Program
     {
         static void Main(string[] args)
         {
+            const string rollbarAccessToken = "17965fa5041749b6bf7095a190001ded";
+            const string rollbarEnvironment = "RollbarNetSamples";
+            var rollbarConfig = new RollbarConfig(rollbarAccessToken)
+            {
+                Environment = rollbarEnvironment,
+            };
+
             var log = new LoggerConfiguration()
-                 .MinimumLevel.Information()
-                 .WriteTo.Console()
-                 .CreateLogger();
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.RollbarSink(rollbarConfig, TimeSpan.FromSeconds(3))
+                .CreateLogger();
 
             var position = new { Latitude = 25, Longitude = 134 };
             var elapsedMs = 34;
