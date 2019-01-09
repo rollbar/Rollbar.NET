a6b29f513b629c77e99572b70bd8e9ceef32e27a
Andrey Kornich <akornich@gmail.com>
2019-01-08 22:37:19 -0800

    - ref #259: Prototype Azure build pipeline for Rollbar.NET - ref #260: Fix unit test failure within Azure Pipeline

--- UnitTest.Rollbar/PayloadTruncation/PayloadTruncationFixture.cs
+++ UnitTest.Rollbar/PayloadTruncation/PayloadTruncationFixture.cs
@@ -102,7 +102,10 @@ namespace UnitTest.Rollbar.PayloadTruncation
             Payload[] payloadsWithFrames = testPayloads.Reverse().Take(2).ToArray();
             foreach(var payload in payloadsWithFrames)
             {
-                AssertPayloadSizeReduction(payload.Data.Body.Trace?.Frames.Length > 1 || payload.Data.Body.TraceChain.Any(trace => trace?.Frames?.Length > 1), payload, trancationStrategy);
+                bool hasFramesToTrim = false;
+                hasFramesToTrim |= (payload?.Data?.Body?.Trace?.Frames?.Length > 1);
+                hasFramesToTrim |= ((payload?.Data?.Body?.TraceChain != null) && payload.Data.Body.TraceChain.Any(trace => trace?.Frames?.Length > 1));
+                AssertPayloadSizeReduction(hasFramesToTrim, payload, trancationStrategy);
                 AssertPayloadSizeReduction(false, payload, trancationStrategy);
             }
             using (var logger = RollbarFactory.CreateNew().Configure(this._config))
