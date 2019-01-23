0945241d789ac10120566d3a444443d7cd354b49
Andrey Kornich <akornich@gmail.com>
2019-01-23 15:20:35 -0800

    - ref #266: Create sample for Rollbar.PlugIns.MSEnterpriseLibrary   Starting Point

---
+++ Samples/Sample.PlugIns.MSEnterpriseLibrary/ConsoleApp/SalaryCalculator .cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class SalaryCalculator
+    {
+        public Decimal GetWeeklySalary(string employeeId, int weeks)
+        {
+            string employeeName = String.Empty;
+            Decimal salary = 0;
+            Decimal weeklySalary = 0;
+
+            //Program.ExceptionManager.Process(() => {
+            //    string connString = ConfigurationManager.ConnectionStrings
+            //                                      ["EmployeeDatabase"].ConnectionString;
+            //    // Access database to get salary for employee.
+            //    // In this example, just assume it's some large number.
+            //    employeeName = "John Smith";
+            //    salary = 1000000;
+            //    weeklySalary = salary / weeks;
+            //}, Program.ExceptionShieldingPolicyID);
+
+            //string connString = ConfigurationManager.ConnectionStrings
+            //                                  ["EmployeeDatabase"].ConnectionString;
+            // Access database to get salary for employee.
+            // In this example, just assume it's some large number.
+            employeeName = "John Smith";
+            salary = 1000000;
+            weeklySalary = salary / weeks;
+
+
+            return weeklySalary;
+        }
+
+        //public Decimal GetWeeklySalary(string employeeId, int weeks)
+        //{
+        //    String connString = string.Empty;
+        //    String employeeName = String.Empty;
+        //    Decimal salary = 0;
+        //    try
+        //    {
+        //        connString = ConfigurationManager
+        //            .ConnectionStrings["EmployeeDatabase"]
+        //            .ConnectionString;
+        //        // Access database to get salary for employee here...
+        //        // In this example, just assume it's some large number.
+        //        employeeName = "John Smith";
+        //        salary = 1000000;
+        //        return salary / weeks;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // provide error information for debugging
+        //        string template = "Error calculating salary for {0}."
+        //                        + " Salary: {1}. Weeks: {2}\n"
+        //                        + "Data connection: {3}\n{4}";
+        //        Exception informationException = new Exception(
+        //            string.Format(template, employeeName, salary, weeks,
+        //                          connString, ex.Message));
+        //        throw informationException;
+        //    }
+        //}
+    }
+}
---
+++ Samples/Sample.PlugIns.MSEnterpriseLibrary/ReadMe.md
@@ -0,0 +1,19 @@
+# Sample: Rollbar.NET via MS Enterprise Library's Exception Handling Block
+
+This example demonstrates how to integrate Rollbar.NET within an application that already uses MS Enterprise Library's Exception Handling Block for its exception logging purposes 
+and have all (or some of) its exception logs/traces forwarded/reported to the Rollbar API Service/Dashboard.
+
+## Starting Point
+
+In this example, we have a very simple .NET console application that already makes use of MS Enterprise Library's Exception Handling Block.
+
+## Adding Logs Forwarding to the Rollbar Dashboard
+
+
+## Done!
+
+Have fun taking advantages for Rollbar Dashboard while monitoring behavior of your products!
+
+Here is an example of an error log from this sample rendered within the Rollbar Dashboard:
+
+
