e8b750c1fc93941032caea103b9ebb7d3ec4b9ab
Andrey Kornich <akornich@gmail.com>
2019-02-27 13:12:35 -0800

    - resolve #325: Extract integration with .NET Framework ASP.NET MVC into separate project (Rollbar.Net.AspNet.Mvc).       - resolve #326: Extract integration with .NET Framework ASP.NET Web API into separate project (Rollbar.Net.AspNet.WebApi).

---
+++ UnitTest.Rollbar.Net.AspNet.WebApi/RollbarExceptionFilterAttributeFixture.cs
@@ -0,0 +1,13 @@
+namespace UnitTest.Rollbar.Net.AspNet.WebApi
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RollbarExceptionFilterAttributeFixture
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+        }
+    }
+}
