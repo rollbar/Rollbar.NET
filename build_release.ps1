c95855347dfad28048e8b8151e4a48fc202f33af
Andrey Kornich <akornich@gmail.com>
2019-03-27 23:49:28 -0700

    - resolve #352: Create automated release build script

--- build_release.ps1
+++ build_release.ps1
@@ -1,3 +1,6 @@
+# That is the location where this script run results will be placed:
+$releasesRoot = "../Rollbar.NET-Releases"
+
 $ErrorActionPreference="Stop"
 $ProgressPreference="SilentlyContinue"
 
@@ -42,23 +45,28 @@ $LocalDotnet = "$InstallDir/dotnet"
 # Run the build process now. Implement your build script here:
 #=============================================================
 
+Write-Host "Harvesting all the SDK module projects..."
+$sdkProjects = 
+    Get-ChildItem -Path "." -Directory | 
+    Where-Object {$_.Name -match '^Rollbar*'} |
+    Where-Object {!($_.Name -match '^Rollbar.Benchmarker')} # we are not interested it this one...
+Write-Host "  " $sdkProjects
+
 Write-Host "Deleting /obj and /bin folders of the SDK projects/modules..."
-Get-ChildItem -Path "." -Directory |
-Where-Object {$_.Name -match '^Rollbar*'} |
-ForEach-Object {
-    Write-Host "  SDK module: " $_.FullName
-    $path = $_.FullName + '/obj'
+foreach($project in $sdkProjects) {
+    Write-Host "  SDK module:" $project.FullName
+    $path = $project.FullName + '/obj'
     if (Test-Path $path) {
     Remove-Item -Path $path -Recurse #-WhatIf
     }
-    $path = $_.FullName + '/bin'
+    $path = $project.FullName + '/bin'
     if (Test-Path $path) {
         Remove-Item -Path $path -Recurse #-WhatIf
     }
 }
 
 Write-Host "Restoring the SDK dependencies..."
-dotnet restore rollbar.sln
+#!!! dotnet restore rollbar.sln
 
 Write-Host "Building all the SDK build configurations..."
 $buildConfigurations = @(
@@ -67,10 +75,12 @@ $buildConfigurations = @(
     "Release"
 )
 foreach($buildConfiguration in $buildConfigurations) {
-    Write-Host "  Building the SDK build configuration:" + $buildConfiguration + ...
+    Write-Host "  Building the SDK build configuration:" $buildConfiguration "..."
     # clean the configuration:
+    <#
     Write-Host "    - cleaning..."
     dotnet clean rollbar.sln --configuration $buildConfiguration
+    #>
     # build the configurations:
     Write-Host "    - building..."
     dotnet build rollbar.sln --configuration $buildConfiguration
@@ -79,10 +89,101 @@ foreach($buildConfiguration in $buildConfigurations) {
     #dotnet test rollbar.sln --configuration $buildConfiguration
 }
 # unit-test the Release configuration: 
-Write-Host "    - unit-testing Debug build..."
-dotnet test rollbar.sln --configuration Debug
+#!!! Write-Host "    - unit-testing Debug build..."
+#!!! dotnet test rollbar.sln --configuration Debug
+
+Write-Host "Consolidating SDK build results..."
+# define location for releases:
+$releasesPath = $releasesRoot
+if(!(Test-Path $releasesPath)) {
+    New-Item -Path $releasesPath -ItemType Directory #-WhatIf
+}
+Write-Host "Releases folder: "$releasesPath
+
+# define location for old releases archive:
+$releasesArchivePath = Join-Path -Path $releasesPath -ChildPath "ARCHIVE" 
+if(!(Test-Path $releasesArchivePath)) {
+    New-Item -Path $releasesArchivePath -ItemType Directory #-WhatIf
+}
+Write-Host "Releases Archive folder: "$releasesArchivePath
+# archive last release:
+Get-ChildItem -Path $releasesPath -Directory |
+Where-Object {$_.Name -match '^v*'} |
+Where-Object {!($_.Name -match '^ARCHIVE')} # we are not interested it this one...
+ForEach-Object {
+	if ($_ -ne $null) {
+		Write-Host "Moving "$_.FullName"..."
+		Move-Item -Path ($_.FullName) -Destination $releasesArchivePath -Force #-WhatIf
+	}
+}
+
+# collect current release:
+Add-Type -assembly "system.io.compression.filesystem"
+
+foreach($project in $sdkProjects) {
+    # project source path:
+    $projectBin = $project.FullName + '/bin'
+
+    # release bin name based on the NuGet package name prefix:
+    $nugetPackage = (Get-ChildItem -Path $projectBin/Release -Filter *.nupkg | Select-Object -First 1).FullName
+    $nugetPackage = [System.IO.Path]::GetFileNameWithoutExtension($nugetPackage)
+    Write-Host "Package:" $nugetPackage
+    $nugetPackageNameComponents =  $nugetPackage.Split('.')
+    $releaseBinName = ""
+    for($i=0;$i -lt ($nugetPackageNameComponents.Count - 3); $i++) {
+        $releaseBinName = $releaseBinName + $nugetPackageNameComponents[$i]
+        if ($i -lt ($nugetPackageNameComponents.Count - 3 - 1)) {
+            $releaseBinName = $releaseBinName + "."
+        }
+    }
+    Write-Host "New release bin name:" $releaseBinName
+
+    # release bin version based on the NuGet package version suffix:
+    [array]::Reverse($nugetPackageNameComponents)
+    Write-Host "Package components:" $nugetPackageNameComponents
+    $releaseBinVersion = 
+        'v' + $nugetPackageNameComponents[2] + '.' + $nugetPackageNameComponents[1] + '.' + $nugetPackageNameComponents[0]
+    Write-Host "Package version:" $releaseBinVersion
+
+    # release folder path:
+    $releaseFolder = Join-Path -Path $releasesPath  -ChildPath $releaseBinVersion 
+    if(!(Test-Path $releaseFolder)) {
+        New-Item -Path $releaseFolder -ItemType Directory #-WhatIf
+    }
+
+    # release bin folder path:
+    $releaseBin = Join-Path -Path $releaseFolder -ChildPath ('bin-' + $releaseBinVersion + '-' + $releaseBinName)
+    if(!(Test-Path $releaseBin)) {
+        New-Item -Path $releaseBin -ItemType Directory #-WhatIf
+    }
+
+    # copy release bin content from the project source path:
+    Copy-Item -Path ($projectBin+'/*') -Destination $releaseBin -recurse -Force #-Verbose
+
+    # stash away NuGet packages of interest:
+    $nugetBuilds = @("Release", "Debug")
+    foreach($nugetBuild in $nugetBuilds) {
+        $releaseNuGetPackagesPath = Join-Path -Path $releaseFolder -ChildPath ("NuGet-"+$nugetBuild)
+        if(!(Test-Path $releaseNuGetPackagesPath)) {
+            New-Item -Path $releaseNuGetPackagesPath -ItemType Directory #-WhatIf
+        }
+        Copy-Item -Path ($releaseBin+'/'+$nugetBuild+'/*.nupkg') -Destination $releaseNuGetPackagesPath -Recurse -Force #-Verbose
+    }
+
+    # zip release bins folders
+    # https://devblogs.microsoft.com/scripting/use-powershell-to-create-zip-archive-of-folder/
+    $zipPath = Join-Path -Path $releaseFolder -ChildPath ((Split-Path $releaseBin -Leaf) + '.zip') 
+    [io.compression.zipfile]::CreateFromDirectory($releaseBin, $zipPath) 
+
+    # delete release bins folders
+    Remove-Item -Path $releaseBin -Recurse -Force #-Verbose
+}
+
+
+
 
 # make sure all the samples are buildable:
+<# 
 Get-ChildItem "./Samples" -Filter *.sln | 
 Foreach-Object {
 
@@ -95,4 +196,5 @@ Foreach-Object {
 
     # build the Release configurations:
     dotnet build $_.FullName --configuration Release | Where-Object {$_.FullName -match 'Xamarin'}
-}
+} 
+#>
