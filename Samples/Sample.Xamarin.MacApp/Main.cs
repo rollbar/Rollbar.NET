844a24c2ad016bf304a3cd59da5f17ca257ef6c2
Andrey Kornich <akornich@gmail.com>
2019-12-04 12:05:25 -0800

    docs: ref #483: Create Sample.Xamarin.MacApp with VS for Mac
    - initial application created from a VS for Mac project template

---
+++ Samples/Sample.Xamarin.MacApp/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Sample.Xamarin.MacApp
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
