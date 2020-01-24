a9a90fd3ad0cf7d5323817033a40057f2762adae
Andrey Kornich <akornich@gmail.com>
2020-01-24 10:46:12 -0800

    chore: resolve #490: Colocate internal infrastructure types

---
+++ Rollbar/InternalInfrastructure/IPayloadQueuesRegistry.cs
@@ -0,0 +1,14 @@
+﻿namespace Rollbar.InternalInfrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal interface IPayloadQueuesRegistry
+    {
+        void Register(PayloadQueue queue);
+        void Unregister(PayloadQueue queue);
+        IReadOnlyCollection<PayloadBundle> PayloadQueues { get; }
+        int PayloadQueuesCount { get; }
+    }
+}
--- Rollbar/RollbarQueueController.cs
+++ Rollbar/RollbarQueueController.cs
@@ -10,17 +10,14 @@ namespace Rollbar
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     using System.Diagnostics;
     using System.Linq;
-    using System.Net;
     using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
     using Newtonsoft.Json;
     using PayloadStore;
     using Serialization.Json;
-    using System.IO;
 
 #if NETFX
     using System.Web.Hosting;
