ea5b2723fdff84f0e1df9dc11f974d3ad43c7adb
Andrey Kornich <akornich@gmail.com>
2020-03-05 18:39:37 -0800

    test: resolve #500: Add CustomKeyValuePackageDecoratorFixture

--- UnitTest.Rollbar/PayloadPackageDecorators/HttpRequestMessagePackageDecoratorFixture.cs
+++ UnitTest.Rollbar/PayloadPackageDecorators/HttpRequestMessagePackageDecoratorFixture.cs
@@ -1,6 +1,6 @@
 ﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-namespace UnitTest.Rollbar.DTOs
+namespace UnitTest.Rollbar.PayloadPackageDecorators
 {
     using global::Rollbar;
     using global::Rollbar.DTOs;
