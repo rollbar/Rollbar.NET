868baded550b4a21a4c4ca3e1d11ca6200aa5753
Andrey Kornich <akornich@gmail.com>
2020-05-14 13:25:34 -0700

    refactor: ref #526
    - refactor: ref #526: Split out Deploys related functionality into separate deployable module
    - created relevant projects for the new module

---
+++ Rollbar.Deploys/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Rollbar.Deploys
+{
+    public class Class1
+    {
+    }
+}
---
+++ UnitTest.Rollbar.Deploys/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Rollbar.Deploys
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
