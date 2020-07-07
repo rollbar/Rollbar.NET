08e4090c5fbf18a170a90480615b0e07c3c47551
Andrey Kornich <akornich@gmail.com>
2020-07-06 18:34:02 -0700

    added ObjectUtility

---
+++ Rollbar/Common/ObjectUtility.cs
@@ -0,0 +1,64 @@
+﻿namespace Rollbar.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+
+    /// <summary>
+    /// Class ObjectUtility.
+    /// </summary>
+    public static class ObjectUtility
+    {
+        /// <summary>
+        /// Ares the similar references.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool AreSimilarReferences(object left, object right)
+        {
+            if(left == right)
+            {
+                return true;
+            }
+            else if(left == null && right == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Ares the comparable via properties.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool AreComparableViaProperties(object left,object right)
+        {
+            if (!(left != null && right !=null))
+            {
+                return false;
+            }
+
+            var leftType = left.GetType();
+            var rightType = right.GetType();
+
+            if (ReflectionUtility.GetTopCommonSuperType(leftType, rightType) != typeof(object))
+            {
+                return true;
+            }
+
+            if (ReflectionUtility.GetCommonImplementedInterfaces(leftType, rightType).Length > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTest.Rollbar/Common/ObjectUtilityFixture.cs
@@ -0,0 +1,88 @@
+﻿namespace UnitTest.Rollbar.Common
+{
+    using global::Rollbar;
+    using global::Rollbar.Common;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    /// <summary>
+    /// Defines test class StringUtilityFixture.
+    /// </summary>
+    [TestClass]
+    [TestCategory(nameof(ObjectUtilityFixture))]
+    public class ObjectUtilityFixture
+    {
+        /// <summary>
+        /// Setups the fixture.
+        /// </summary>
+        [TestInitialize]
+        public void SetupFixture()
+        {
+        }
+
+        /// <summary>
+        /// Tears down fixture.
+        /// </summary>
+        [TestCleanup]
+        public void TearDownFixture()
+        {
+        }
+
+        [TestMethod]
+        public void TestAreSimilarReferences()
+        {
+            object anObject = new object();
+            object anotherObject = anObject;
+            object nullObject = null;
+
+            Assert.IsTrue(ObjectUtility.AreSimilarReferences(anObject, anObject));
+            Assert.IsTrue(ObjectUtility.AreSimilarReferences(anObject, anotherObject));
+
+            Assert.IsTrue(ObjectUtility.AreSimilarReferences(nullObject, nullObject));
+            Assert.IsTrue(ObjectUtility.AreSimilarReferences(nullObject, null));
+
+            Assert.IsFalse(ObjectUtility.AreSimilarReferences(nullObject, anotherObject));
+            Assert.IsFalse(ObjectUtility.AreSimilarReferences(anObject, new object()));
+        }
+
+        [TestMethod]
+        public void TestAreComparableViaProperties()
+        {
+            Assert.IsFalse(ObjectUtility.AreComparableViaProperties(null, null));
+            Assert.IsFalse(ObjectUtility.AreComparableViaProperties(new object(), null));
+
+            Assert.IsFalse(ObjectUtility.AreComparableViaProperties(new AClass(), new BClass()));
+            Assert.IsFalse(ObjectUtility.AreComparableViaProperties(new ASubclass(), new BSubclass()));
+            Assert.IsTrue(ObjectUtility.AreComparableViaProperties(new AClass(), new AClass()));
+            Assert.IsTrue(ObjectUtility.AreComparableViaProperties(new AClass(), new ASubclass()));
+
+            Assert.IsFalse(ObjectUtility.AreComparableViaProperties(new IntAClass(), new IntBClass()));
+            Assert.IsTrue(ObjectUtility.AreComparableViaProperties(new IntAClass(), new MultifacedClass()));
+        }
+
+    }
+
+
+    #region mocking birds
+
+    public class AClass {}
+    public class BClass {}
+
+    public class ASubclass : AClass {}
+    public class BSubclass : BClass {}
+
+    public interface IntA { }
+    public interface IntB { }
+
+    public class IntAClass : IntA {}
+    public class IntBClass : IntB {}
+    public class MultifacedClass : IntA, IntB {}
+
+
+    #endregion mocking birds
+
+}
