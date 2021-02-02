d72df6177fe194acd0f6db5da00997eaf537b85c
Andrey Kornich <akornich@gmail.com>
2021-02-02 15:26:36 -0700

    Sample.Net.ConsoleApp.AppSettings.Json - initial

---
+++ Samples/Sample.Net.ConsoleApp.AppSettings.Json/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Net.ConsoleApp.AppSettings.Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
