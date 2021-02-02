61465201b8a97d085f7dbcf82637abde3005efbd
Andrey Kornich <akornich@gmail.com>
2021-02-02 16:03:15 -0700

    Sample.Net.ConsoleApp.AppSettings.Json - basic Rollbar integration

--- Samples/Sample.Net.ConsoleApp.AppSettings.Json/Program.cs
+++ Samples/Sample.Net.ConsoleApp.AppSettings.Json/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Rollbar;
+
 namespace Sample.Net.ConsoleApp.AppSettings.Json
 {
     class Program
     {
         static void Main(string[] args)
         {
+            RollbarLocator.RollbarInstance.AsBlockingLogger(TimeSpan.FromSeconds(3))
+                .Info($"{typeof(Program).Namespace} sample: Rollbar Notifier is alive based on appsetting.json!");
         }
     }
 }
