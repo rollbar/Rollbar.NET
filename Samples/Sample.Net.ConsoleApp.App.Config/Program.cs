61f05f09b8f0bdf69409193c46287df8a1ce082e
Andrey Kornich <akornich@gmail.com>
2021-02-02 14:11:57 -0700

    Sample.Net.ConsoleApp.App.Config - basic Rollbar integration

--- Samples/Sample.Net.ConsoleApp.App.Config/Program.cs
+++ Samples/Sample.Net.ConsoleApp.App.Config/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Rollbar;
+
 namespace Sample.Net.ConsoleApp.App.Config
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // The Rollbar Notifier is configured within the app.config.
+            // Let's just start using it:
+            RollbarLocator.RollbarInstance.AsBlockingLogger(TimeSpan.FromSeconds(3))
+                .Info($"{typeof(Program).Namespace} sample: Rollbar is alive based on the app.config settings!.");
         }
     }
 }
