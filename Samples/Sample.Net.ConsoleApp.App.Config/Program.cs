7f500cdeebb00fe26c61cc7ed8a5566f0efc6779
Andrey Kornich <akornich@gmail.com>
2021-08-04 14:34:39 -0700

    updating samples

--- Samples/Sample.Net.ConsoleApp.App.Config/Program.cs
+++ Samples/Sample.Net.ConsoleApp.App.Config/Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Rollbar;
-using Rollbar.App.Config;
-
-namespace Sample.Net.ConsoleApp.App.Config
+﻿namespace Sample.Net.ConsoleApp.App.Config
 {
+    using System;
+
+    using Rollbar;
+    using Rollbar.App.Config;
+
     class Program
     {
         static void Main(string[] args)
@@ -17,8 +13,6 @@ namespace Sample.Net.ConsoleApp.App.Config
             AppConfigUtility.LoadAppSettings(rollbarInfrastructureConfig);
             RollbarInfrastructure.Instance.Init(rollbarInfrastructureConfig);
 
-            // The Rollbar Notifier is configured within the app.config.
-            // Let's just start using it:
             RollbarLocator.RollbarInstance.AsBlockingLogger(TimeSpan.FromSeconds(3))
                 .Info($"{typeof(Program).Namespace} sample: Rollbar is alive based on the app.config settings!");
         }
