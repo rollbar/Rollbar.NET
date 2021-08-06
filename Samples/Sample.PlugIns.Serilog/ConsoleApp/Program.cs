b58cebbef34f84bcd2e76fef6ee52754c5883158
Andrey Kornich <akornich@gmail.com>
2021-08-05 21:56:58 -0700

    updating samples

--- Samples/Sample.PlugIns.Serilog/ConsoleApp/Program.cs
+++ Samples/Sample.PlugIns.Serilog/ConsoleApp/Program.cs
@@ -4,24 +4,24 @@
     using Serilog;
     using Rollbar;
     using Rollbar.PlugIns.Serilog;
+    using Samples;
 
     public class Program
     {
         static void Main(string[] args)
         {
-            // Define RollbarConfig:
-            const string rollbarAccessToken = "17965fa5041749b6bf7095a190001ded";
-            const string rollbarEnvironment = "RollbarNetSamples";
-            RollbarConfig rollbarConfig = new RollbarConfig(rollbarAccessToken)
-            {
-                Environment = rollbarEnvironment,
-            };
+            // Define RollbarInfrastructureConfig:
+            RollbarInfrastructureConfig rollbarInfrastructureConfig = 
+                new RollbarInfrastructureConfig(
+                    RollbarSamplesSettings.AccessToken, 
+                    RollbarSamplesSettings.Environment
+                    );
 
             // Add RollbarSink to the Serilog Logger using pre-configured RollbarConfig:
             Serilog.Core.Logger log = new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .WriteTo.Console()
-                .WriteTo.RollbarSink(rollbarConfig, TimeSpan.FromSeconds(3))
+                .WriteTo.RollbarSink(rollbarInfrastructureConfig, TimeSpan.FromSeconds(3))
                 .CreateLogger();
 
             var position = new { Latitude = 25, Longitude = 134 };
