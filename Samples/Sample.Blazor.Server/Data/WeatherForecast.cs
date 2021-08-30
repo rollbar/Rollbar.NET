992ac7496990d56f5a22934df1258ed7aed7dfcc
Andrey Kornich <akornich@gmail.com>
2021-08-30 12:12:53 -0700

    Sample.Blazor.Server - starting point

---
+++ Samples/Sample.Blazor.Server/Data/WeatherForecast.cs
@@ -0,0 +1,24 @@
+namespace Sample.Blazor.Server.Data
+{
+    using System;
+
+    public class WeatherForecast
+    {
+        public DateTime Date
+        {
+            get; set;
+        }
+
+        public int TemperatureC
+        {
+            get; set;
+        }
+
+        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+        public string Summary
+        {
+            get; set;
+        }
+    }
+}
