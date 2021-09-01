8af0b4d0515a60cea9450322b8fa15ef601cc714
Andrey Kornich <akornich@gmail.com>
2021-09-01 12:07:29 -0700

    integrated Rollbar within Sample.AspNetCore.WebApp

--- Samples/Sample.AspNetCore.WebApp/Startup.cs
+++ Samples/Sample.AspNetCore.WebApp/Startup.cs
@@ -7,10 +7,17 @@ namespace Sample.AspNetCore.WebApp
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.HttpsPolicy;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    using Rollbar;
+    using Rollbar.NetCore.AspNet;
+
+    using Samples;
 
     public class Startup
     {
@@ -27,6 +34,19 @@ namespace Sample.AspNetCore.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // STEP.1 - enable Http Context Accessor:
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // STEP.2 - Setup Rollbar Infrastructure:
+            ConfigureRollbarInfrastructure();
+
+            // STEP.3 - Add Rollbar logger with properly configured log level filter:
+            services.AddRollbarLogger(loggerOptions =>
+            {
+                loggerOptions.Filter =
+                  (loggerName, loglevel) => loglevel >= LogLevel.Trace;
+            });
+
             services.AddRazorPages();
         }
 
@@ -44,6 +64,11 @@ namespace Sample.AspNetCore.WebApp
                 app.UseHsts();
             }
 
+            // STEP.4 - Use Rollbar middleware:
+            app.UseRollbarMiddleware();
+            // Any other middleware component intended to be "monitored" by Rollbar middleware
+            // go below this line:
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -56,5 +81,59 @@ namespace Sample.AspNetCore.WebApp
                 endpoints.MapRazorPages();
             });
         }
+
+        // STEP.2 - Setup Rollbar Infrastructure:
+        private void ConfigureRollbarInfrastructure()
+        {
+            RollbarInfrastructureConfig config = new RollbarInfrastructureConfig(
+              RollbarSamplesSettings.AccessToken,
+              RollbarSamplesSettings.Environment
+            );
+            RollbarDataSecurityOptions dataSecurityOptions = new RollbarDataSecurityOptions();
+            dataSecurityOptions.ScrubFields = new string[]
+            {
+      "url",
+      "method",
+            };
+            config.RollbarLoggerConfig.RollbarDataSecurityOptions.Reconfigure(dataSecurityOptions);
+
+            RollbarInfrastructure.Instance.Init(config);
+
+            // Optionally:
+            RollbarInfrastructure.Instance.QueueController.InternalEvent += OnRollbarInternalEvent;
+        }
+
+        // STEP.2 - Setup Rollbar Infrastructure:
+        private static void OnRollbarInternalEvent(object sender, RollbarEventArgs e)
+        {
+            Console.WriteLine(e.TraceAsString());
+
+            RollbarApiErrorEventArgs apiErrorEvent = e as RollbarApiErrorEventArgs;
+            if(apiErrorEvent != null)
+            {
+                //TODO: handle/report Rollbar API communication error event...
+                return;
+            }
+            CommunicationEventArgs commEvent = e as CommunicationEventArgs;
+            if(commEvent != null)
+            {
+                //TODO: handle/report Rollbar API communication event...
+                return;
+            }
+            CommunicationErrorEventArgs commErrorEvent = e as CommunicationErrorEventArgs;
+            if(commErrorEvent != null)
+            {
+                //TODO: handle/report basic communication error while attempting to reach Rollbar API service... 
+                return;
+            }
+            InternalErrorEventArgs internalErrorEvent = e as InternalErrorEventArgs;
+            if(internalErrorEvent != null)
+            {
+                //TODO: handle/report basic internal error while using the Rollbar Notifier... 
+                return;
+            }
+
+        }
+
     }
 }
