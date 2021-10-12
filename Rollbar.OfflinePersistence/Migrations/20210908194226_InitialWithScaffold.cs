579fa98ee42a5310cc2659acb09b486a6049881d
Andrey Kornich <akornich@gmail.com>
2021-10-11 17:06:07 -0700

    fix: [CC] clear VS2022 build warnings and messages for the module - Rollbar.OfflinePersistence

--- Rollbar.OfflinePersistence/Migrations/20210908194226_InitialWithScaffold.cs
+++ Rollbar.OfflinePersistence/Migrations/20210908194226_InitialWithScaffold.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Rollbar.Migrations
 {
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class InitialWithScaffold : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -15,3 +17,5 @@ namespace Rollbar.Migrations
         }
     }
 }
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
